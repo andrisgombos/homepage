ac32b0213509a0a612b609bdc30abdb9ab00bbfe
Andras <andrisgombos@gmail.com>
2022-02-04 13:42:52 +0100

    header navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Navbar = styled.div`
+    npmcolor: palevioletred;
+`
 
 const Header = () => (
-    <title>GA</title>
-)
+  <>
+    <Navbar>
+        <h1>GA</h1>
+        <ul>
+            <li>about me</li>
+            <li>resume</li>
+            <li>projects</li>
+            <li>contact</li>
+        </ul>
+    </Navbar>
+  </>
+);
 
 export default Header;
