a9f88f1ace631617a4e9b3211764c3eff7560f28
Andras <andrisgombos@gmail.com>
2022-02-04 14:37:23 +0100

    main page start

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Navbar = styled.div`
-    npmcolor: palevioletred;
+
+// Styling details for the navbar
+
+const Navbar = styled.div` 
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    height: 3.5em;
+    left: 0;
+    top: 0;
+    position: fixed;
+    z-index: 100;
+    border-bottom: solid 1px #2c2c2c;
+    
+    h1 {
+        display: inline;
+        font-size: 0.9em;
+        padding: 0 0 0 1.5em;
+    }
+
+    ul {
+        width: 100%;
+        left: 0;
+        display: flex;
+        color: blue;
+        list-style: none;
+        }
+    li {
+        margin-left: 1.5em;
+        display: inline-block;
+        padding-left: 0;
+        color: blue;
+    }
+    
 `
 
 const Header = () => (
   <>
     <Navbar>
-        <h1>GA</h1>
+        <h1>Gombos_Andras</h1>
         <ul>
             <li>about me</li>
             <li>resume</li>
