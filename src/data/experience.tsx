704f9df4c7ffe942fda409b4be6dd3e478c8ebe6
Andras <andrisgombos@gmail.com>
2022-02-07 18:25:49 +0100

    data update for resume

---
+++ src/data/experience.tsx
@@ -0,0 +1,45 @@
+const experience = [
+    {
+        company: 'NatWest Services (Switzerland) Ltd',
+        position: 'IT Project Analyst',
+        link: 'https://www.force-7.ch/',
+        date: 'November 2021 - February 2022',
+        points: ''
+    },
+    {
+        company: 'SIT Academy (formerly Propulsion Academy)',
+        position: 'Full-Stack Developer',
+        link: 'https://sit.academy/',
+        date: 'February 2021 - May 2022',
+        points: [
+            'Developed frontend projects using React & Redux and worked in various teams with Git and Gitlab',
+            'Backend project creation and development with Python and Django (REST API) using Docker and Docker-compose',
+            'Took the lead on the functionality and design of the GOES App that was developed as a final group project',
+        ]
+    },
+    {
+        company: 'Diageo',
+        position: 'Pricing Controller',
+        link: 'https://www.diageo.com/',
+        date: 'November 2016 - December 2020',
+        points: [
+            'Performed root cause analysis on pricing corrections,  fixed gaps, and minimize corrections by 15%',
+            'Established and managed strong relationship with customers and internal stakeholders',
+            'Set up and validated prices in SAP system for over 83 customers across 24 countries.',
+            'Managed internal and external audits by improving workflow',
+        ]
+    },
+    {
+        company: 'Diageo',
+        position: 'Credit Controller',
+        link: 'https://www.diageo.com/',
+        date: 'November 2014 - October 2016',
+        points: [
+            'AR and AP controlling, credit management for over 100 clients across the globe.',
+            'Accurate cash forecasting and risk mitigation',
+            'Standardized processes around the Global Travel to ensure ways of working can be easily transferred',
+        ]
+    },
+];
+
+export default experience;
---
+++ src/data/resume.tsx
@@ -0,0 +1,16 @@
+const degrees = [
+    {
+        school: 'SIT Academy (formerly Propulsion Academy)',
+        degree: 'Full-Stack Developer Bootcamp',
+        link: 'https://sit.academy/',
+        year: '2021',
+    },
+    {
+        school: 'Corvinus University of Budapest',
+        degree: 'BA in Communication and Media Studies',
+        link: 'https://www.uni-corvinus.hu/?lang=en',
+        year: '2008-2012',
+    },
+];
+
+export default degrees;
