498c9f251bda33d2cd1e58bdf22ce4d26b3e3340
Andras <andrisgombos@gmail.com>
2022-02-16 18:00:42 +0100

    design updates for projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 import { DataContainer, GlobalBackground } from '../styles/globalstyles';
 import Header from './header';
 import goesvideo from '../assets/goesvideo.mp4';
+import soc1 from '../assets/motion1.png'
+import soc2 from '../assets/motion2.png'
 
 const ProjectContainer = styled.div`
     width: 50%;
     border-right: 2px solid #F5DF4E;
+    border-bottom: 2px solid #F5DF4E;
     padding: 2em;
     display: flex;
     flex-direction: column;
@@ -15,6 +18,18 @@ const ProjectContainer = styled.div`
 const VideoContainer = styled.video`
     width: 500px;
     height: 300px;
+`
+
+const PhotoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+
+    img {
+        width: 460px;
+        height: 300px;
+    }
 `
 
 const Projects = () => (
@@ -30,9 +45,18 @@ const Projects = () => (
                 </ProjectContainer>
                 <ProjectContainer>
                     <h3>Social Media Site</h3>
-                    
+                    <PhotoContainer>
+                        <img src={soc2} alt='social media site preview'/>
+                        <p>Social media site containing both Front and Backend developed in a Docker container.</p>
+                        <p>Users can share thought, pictures, interact with each other's posts. Stack used: React.js, Django, Docker, Nginx.</p>
+                    </PhotoContainer>
                 </ProjectContainer>
             </DataContainer>
+            {/* <DataContainer>
+                <ProjectContainer>
+                    <h3>Photogram</h3>
+                </ProjectContainer>
+            </DataContainer> */}
         </GlobalBackground>
     </>
 )
--- src/styles/globalstyles.tsx
+++ src/styles/globalstyles.tsx
@@ -5,6 +5,7 @@ export const GlobalBackground = styled.div`
     height: 93vh;
     background-color: #F5DF4E;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     box-shadow: inset rgba(0, 0, 0, 0.15) 0px 5px 15px;
@@ -15,6 +16,7 @@ export const DataContainer = styled.div`
     height: 70vh;
     background-color: #F5F5F5;
     display: flex;
+    flex-direction: row;
     justify-content: center;
     border: 1px solid rgba(211, 211, 211, .5);
     box-shadow: rgba(0, 0, 0, 0.15) 0px 5px 15px;
